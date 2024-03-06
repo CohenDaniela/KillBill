a880ca8562480627852a11c76a68ecf1ba3d2b23
Daniela Cohen <danielaruthcohen@gmail.com>
2024-03-06 10:46:08 +0100

    uninstall dependencies  without use

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2/promise");
 require("dotenv").config();
-const jwt = require('jsonwebtoken');
-const bcrypt = require("bcrypt");
+
 
 
 ///crear servidor
